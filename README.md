e58cd4862c551b15d7deb2fb78d3ff92b7c5dce9
danielstorey <danielstorey@gmail.com>
2016-10-13 14:27:38 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-# stacklist
+# adapt-stacklist
 
-## Coming Soon
+**Stacklist** is a *presentation component* Created by Dan storey.
+<img align="right" src="https://raw.githubusercontent.com/danielstorey/assets/master/stacklist-demo.png" alt="Stacklist in action">
+
+The **Stacklist** component displays list items one at a time when the learner clicks a button. Items fly into view from left and right alternately.
+
+##Installation
+
+Simply copy this folder into src/components/ and run the `grunt build` command.
+
+## Settings Overview
+
+The attributes listed below are used in *components.json* to configure **Stacklist**, and are properly formatted as JSON in [*example.json*](https://github.com/danielstorey/adapt-stacklist/example.json).
+
+### Attributes
+
+For core model attributes see [**core model attributes**](https://github.com/adaptlearning/adapt_framework/wiki/Core-model-attributes). The attributes listed below are specific to the `Stacklist` component.
+
+**_component** (string): This value must be: `stacklist`.
+
+**_items** (array): An array of text strings, each one representing one list item. Text may include html.
+
+## Limitations
+
+No known limitations
+
+----------------------------
+**Version number:**  1.0
+**Framework versions:**  2.0
+**Author / maintainer:** Dan Storey
---
+++ js/adapt-stacklist.js
@@ -0,0 +1,70 @@
+define(function(require) {
+
+	var ComponentView = require('coreViews/componentView');
+	var Adapt = require('coreJS/adapt');
+
+	var StackList = ComponentView.extend({
+
+		events: {
+			"click .stacklist-next": "nextItem"
+		},
+
+		postRender: function() {
+			this.setupListItems();
+			this.setReadyStatus();
+			this.model.set("_stage", -1);
+		},
+
+		setupListItems: function() {
+
+			// Set item positions alternating R and L
+			var $items = this.$(".stacklist-item");
+			var wWin = $(window).width();
+			$items.each(function(i) {
+				var $el = $items.eq(i);
+				var even = i % 2 === 0;
+				var offset = $el.offset();
+				offset.left = even ? - wWin : wWin;
+				$el.offset(offset);
+			});
+		},
+
+		nextItem: function() {
+			var stage = this.model.get("_stage") + 1;
+			this.setStage(stage);
+		},
+
+		setStage: function(stage) {
+			this.model.set("_stage", stage);
+
+			this.$(".stacklist-next").html("Next");
+			var $item = this.$(".stacklist-item").eq(stage);
+			var h = $item.outerHeight(true);
+
+			this.$(".stacklist-button").css({top: "+=" + h});
+			setTimeout(function() {
+				$item.css({left: 0});
+			}, 250);
+
+			if (this.model.get("_items").length - 1 === stage) {
+				this.onComplete()
+			}
+		},
+
+		onComplete: function () {
+
+			var $button = this.$(".stacklist-button");
+			$button.css({top: $(window).height()});
+			setTimeout(function() {
+				$button.remove();
+			}, 500);
+
+			this.setCompletionStatus();
+		}
+	});
+
+	Adapt.register('stacklist', StackList);
+
+	return StackList;
+
+});
